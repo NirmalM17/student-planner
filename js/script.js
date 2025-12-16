0cd1670b87ac6a1a86a6d7b9df8090a2af920551
NirmalM17 <nirmalm2006@gmail.com>
2025-12-16 19:31:50 +0530

    script

---
+++ js/script.js
@@ -0,0 +1,16 @@
+console.log("Student Productivity System Loaded Successfully");
+function toggleTheme() {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+}
+
+window.onload = () => {
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark");
+    }
+};
